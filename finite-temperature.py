4872eba8461c9e5738abadf6c83d9622b7d59a53
Isaac Tamblyn <isaac.tamblyn@gmail.com>
2016-07-08 17:48:14 -0400

    fixed up formatting, changed to read an xyz rather than store as internal coordinates

--- finite-temperature.py
+++ finite-temperature.py
@@ -1,123 +1,104 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 
-import sys
-
-print 'usage: ' + sys.argv[0] + ' lamda'
-
+import os, sys
 import random
 import numpy
 
-###########################
-#############################
-                            ##
-num_x_trans = 4             ##
-num_y_trans = 4             ##
-num_z_trans = 4             ##
-                            ##
-number_of_snapshots = 1000  ##
-                            ##
+def main():
+    """
+    This code adds thermal noise to a set of atoms
+    """
+    # A clean way to ask for user input
+    try:
+        # Attempt to retrieve required input from user
+        prog = sys.argv[0]
+        input_file = sys.argv[1]
+        lamda = float(sys.argv[2])
+    except IndexError:
+        # Tell the user what they need to give
+        print '\nusage: '+prog+' input_file lamda (amount of thermal noise)\n'
+        # Exit the program cleanly
+        sys.exit(0)
 
-#lattice_constant = 8.1239303805060814         # bcc, 4x4x4
-#lattice_constant = 7.6766331707100584         # fcc, 3x3x3
-#lattice_constant = 9.6719517240988271         #  sc, 2x2x2
+    num_x_trans = 1             
+    num_y_trans = 1             
+    num_z_trans = 1             
+                                
+    number_of_snapshots = 1000  
+                                
+    lattice_constant = 1.0
 
-lattice_constant = 1.0
-                            ##
-#############################
-############################
+    a1 = numpy.array([1.0,0.0,0.0])
+    a2 = numpy.array([0.0,1.0,0.0])
+    a3 = numpy.array([0.0,0.0,1.0])
 
-lamda = float(sys.argv[1])
+    atom_names, basis_atoms = read_xyz(input_file) 
 
-# note that in order to compare structures, density needs to be the same. sc,bcc, fcc check out okay, so the program is probably good!
-# for cI16, with 16 atom basis, you need to put a *4 in denomenator...?
+    number_of_particles = num_x_trans*num_y_trans*num_z_trans*len(basis_atoms)
+    rs = (lattice_constant)*(3/(4*number_of_particles*numpy.pi))**(1.0/3.0)
+    print rs
 
-lattice_constant *= 0.529177
+    sigma_x = lamda  
+    sigma_y = lamda 
+    sigma_z = lamda 
 
+    bravis_lattice = []
+    name_list = []
 
-a1 = numpy.array([1.0,0.0,0.0])
-a2 = numpy.array([0.0,1.0,0.0])
-a3 = numpy.array([0.0,0.0,1.0])
+    for i in range(num_x_trans):
+         for j in range(num_y_trans):
+              for k in range(num_z_trans):
+                   for l in range(len(basis_atoms)):
+                        name_list.append(atom_names[l])
+                        bravis_lattice.append(i*a1 + j*a2 + k*a3 + basis_atoms[l])
 
-sc  =     numpy.array([[0.0, 0.0, 0.0]])
-
-bcc =     numpy.array([[0.0,0.0,0.0], 
-                       [0.5,0.5,0.5]])
-
-fcc =     numpy.array([[0.0,0.0,0.0], 
-                       [0.5,0.5,0.0], 
-                       [0.5,0.0,0.5], 
-                       [0.0,0.5,0.5]])
-
-cI16_JY = numpy.array([[4.0E-02,  4.0E-02,  4.0E-02], 
-                       [4.6E-01,  9.6E-01,  5.4E-01], 
-                       [9.6E-01,  5.4E-01,  4.6E-01],  
-                       [2.9E-01,  2.9E-01,  2.9E-01],  
-                       [5.4E-01,  4.6E-01,  9.6E-01], 
-                       [2.1E-01,  7.1E-01,  7.9E-01],  
-                       [7.1E-01,  7.9E-01,  2.1E-01],  
-                       [7.9E-01,  2.1E-01,  7.1E-01],  
-                       [5.4E-01,  5.4E-01,  5.4E-01],  
-                       [9.6E-01,  4.6E-01,  4.0E-02],  
-                       [4.6E-01,  4.0E-02,  9.6E-01],  
-                       [4.0E-02,  9.6E-01,  4.6E-01],  
-                       [7.9E-01,  7.9E-01,  7.9E-01],  
-                       [7.1E-01,  2.1E-01,  2.9E-01],  
-                       [2.1E-01,  2.9E-01,  7.1E-01],  
-                       [2.9E-01,  7.1E-01,  2.1E-01]])
-
-bc8 =     numpy.array([[0.0E+00,  0.0E+00,  0.0E+00],
-                       [5.0E-01,  0.0E+00,  5.0E-01],
-                       [0.0E+00,  5.0E-01,  5.0E-01],
-                       [5.0E-01,  5.0E-01,  0.0E+00],
-                       [5.0E-01,  5.0E-01,  5.0E-01],
-                       [0.0E+00,  5.0E-01,  0.0E+00],
-                       [5.0E-01,  0.0E+00,  0.0E+00],
-                       [0.0E+00,  0.0E+00,  5.0E-01]])
-
-diamond = numpy.array([[0.00, 0.00, 0.00],
-                       [0.50, 0.50, 0.00],
-                       [0.50, 0.00, 0.50],
-                       [0.00, 0.50, 0.50],
-                       [0.25, 0.25, 0.25],
-                       [0.75, 0.75, 0.25],
-                       [0.75, 0.25, 0.75],
-                       [0.25, 0.75, 0.75]])
-
-basis_atoms = cI16_JY 
-
-number_of_particles = num_x_trans*num_y_trans*num_z_trans*len(basis_atoms)
-rs = (lattice_constant)*(3/(4*number_of_particles*numpy.pi))**(1.0/3.0)
-print rs
-
-sigma_x = lamda  
-sigma_y = lamda 
-sigma_z = lamda 
-
-bravis_lattice = []
-
-for i in range(num_x_trans):
-     for j in range(num_y_trans):
-          for k in range(num_z_trans):
-               for l in range(len(basis_atoms)):
-                    bravis_lattice.append(i*a1 + j*a2 + k*a3 + basis_atoms[l])
-
-filename = 'TRAJEC.' + '%.4f' % lamda + '.xyz'
-#filename = 'TRAJEC.xyz'
-
-outputFile = open(filename,'w')
+    filename = 'TRAJEC.' + '%.4f' % lamda + '.xyz'
 
 
-for time_step in range(number_of_snapshots):
+    outputFile = open(filename,'w')
 
-     outputFile.write(repr(len(bravis_lattice)) + '\n')
-     outputFile.write(repr(time_step + 1) + '\n')
-     
-     for triple in bravis_lattice:
+
+    for time_step in range(number_of_snapshots):
+
+        outputFile.write(repr(len(bravis_lattice)) + '\n')
+        outputFile.write(repr(time_step + 1) + '\n')
+
+        id = 0
+
+        for triple in bravis_lattice:
          
-          xcart = lattice_constant/float(num_x_trans)*triple[0] + random.gauss(0.0, sigma_x)
-          ycart = lattice_constant/float(num_y_trans)*triple[1] + random.gauss(0.0, sigma_y)
-          zcart = lattice_constant/float(num_z_trans)*triple[2] + random.gauss(0.0, sigma_z)
+            xcart = lattice_constant/float(num_x_trans)*triple[0] + random.gauss(0.0, sigma_x)
+            ycart = lattice_constant/float(num_y_trans)*triple[1] + random.gauss(0.0, sigma_y)
+            zcart = lattice_constant/float(num_z_trans)*triple[2] + random.gauss(0.0, sigma_z)
 
-          outputFile.write('H ' + repr(xcart) + '   ' + repr(ycart) + '   ' + repr(zcart) + '\n')
-               
-outputFile.close()
+            name = name_list[id]
+            
+            outputFile.write(name + ' ' + repr(xcart) + '   ' + repr(ycart) + '   ' + repr(zcart) + '\n')
+            id += 1   
+    outputFile.close()
+
+
+def read_xyz(input_filename):
+
+    inputFile = open(input_filename, 'r')
+
+    natoms = int(inputFile.readline())
+    inputFile.readline() # skip comment line
+
+    atom_names = []
+    atom_coords = numpy.zeros((natoms,3),dtype=float)
+
+    for i in range(natoms):
+        line = inputFile.readline()
+        atom_names.append(line.split()[0])
+        atom_coords[i][0] = float(line.split()[1])
+        atom_coords[i][1] = float(line.split()[2])
+        atom_coords[i][2] = float(line.split()[3])
+
+    return atom_names, atom_coords
+
+
+
+# This executes main() only if executed from shell
+if __name__ == '__main__':
+    main()
